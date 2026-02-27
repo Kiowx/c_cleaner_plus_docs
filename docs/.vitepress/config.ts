9c8c48ad90dd769157779afe22e14bbf3b1aa868
Kiowx <157085748+Kiowx@users.noreply.github.com>
2026-02-27 17:45:13 +0800

    Create config.ts

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  lang: 'zh-CN',
+  title: 'C_Cleaner_Plus使用文档',
+  description: '',
+  themeConfig: {
+    nav: [
+      { text: '开始', link: '/guide/' }
+    ]
+  }
+})
